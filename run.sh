0dc7194b994e89ddf0fc41b68100aa8a836c447a
WinstonHChou <winston.h.chou@gmail.com>
2024-10-23 17:10:20 -0700

    mount train.sh to container

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ docker run \
     -v /dev/bus/usb:/dev/bus/usb \
     --device /dev/video0 \
     -v /run/user/1000/gdm/Xauthority:/root/.Xauthority:rw \
+    -v ./train.sh:/root/mycar/train.sh \
     --runtime=nvidia \
     --gpus all \
     donkey_v4:latest
--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 dt=$(date '+%Y-%m-%d_%H%M%S')
-model="model_$dt"
+model="models/model_$dt"
 # echo "python3 -c 'print(\"$model\")'"
 python train.py --model="$model" --type=linear --tubs=data/
