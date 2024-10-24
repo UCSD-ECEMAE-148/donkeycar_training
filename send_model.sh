fae06148657be20a7666b28fa55f014cee6072b3
WinstonHChou <winston.h.chou@gmail.com>
2024-10-23 18:14:34 -0700

    fix send_model.sh

--- send_model.sh
+++ send_model.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 container_name="donkey_v4"
 echo "pulling out models ..."
-docker cp $container_name:/root/mycar/models/ .
+docker cp $container_name:/root/mycar/models/ ./model_output/
