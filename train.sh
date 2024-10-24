e3e833151094b4f6cbe0db37be148337bdd1608c
WinstonHChou <winston.h.chou@gmail.com>
2024-10-23 17:24:29 -0700

    rename model directory to `models/model_$dt.h5`

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 dt=$(date '+%Y-%m-%d_%H%M%S')
-model="models/model_$dt"
-# echo "python3 -c 'print(\"$model\")'"
+model="models/model_$dt.h5"
+echo "python3 -c 'print(\"$model\")'"
 python train.py --model="$model" --type=linear --tubs=data/
